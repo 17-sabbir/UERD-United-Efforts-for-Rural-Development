95303e4b20c71989ff8a71dcb1e49f7c857b169b
17-sabbir <sabbir05m.nstu@gmail.com>
2026-04-15 19:00:10 +0600

    scrollbar

--- resources/views/admin/publications/index.blade.php
+++ resources/views/admin/publications/index.blade.php
@@ -17,7 +17,7 @@
                         <i class="bx bx-plus"></i> Add New Publication
                     </a>
                 </div>
-                <div class="p-4 border rounded table-responsive" style="max-height: calc(100vh - 260px); overflow-y: auto;">
+                <div class="p-4 border rounded table-responsive" style="max-height: calc(100vh - 260px); overflow-y: auto; overflow-x: auto;">
                     <table class="table table-hover table-striped">
                         <thead>
                             <tr>
